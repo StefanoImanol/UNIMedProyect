4dec8d60aff9dbb388f9ab1ffaca46cb1b400a01
Stefano <stefano.ramirez.u@uni.pe>
2024-11-24 14:59:38 -0500

    avance

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -7,9 +8,36 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  email: string = '';
+  password: string = '';
 
+  constructor(private http: HttpClient, private router: Router) {}
   goToRegister() {
     this.router.navigate(['/register']);
   }
+  login() {
+    const payload = {
+      email: this.email,
+      password: this.password
+    };
+    
+    this.http.post('http://127.0.0.1:8000/login/', payload).subscribe({
+      next: (response: any) => {
+        if (response.redirect === 'home-administrador') {
+          this.router.navigate(['/home-administrador']);
+        } else if (response.redirect === 'home-paciente') {
+          this.router.navigate(['/home-paciente']);
+        } else if (response.redirect === 'home-medico') {
+          this.router.navigate(['/home-medico']);
+        }
+      },
+      error: (error) => {
+        console.error('Error en el inicio de sesión', error);
+        alert('Credenciales incorrectas o error de conexión.');
+      }
+      
+    });
+  }
 }
+
+
